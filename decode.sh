227ef951e2055009d1ebfa288fab7014392552ef
Pavel Denisov <pavel.denisov@ims.uni-stuttgart.de>
2018-11-28 17:58:09 +0100

    Initial commit

---
+++ decode.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+file=$1
+language=$2
+recid=$(basename $file | sed 's/\.[^.]*$//g')
+
+basedir=${PWD}
+segmenter=${basedir}/kaldi-offline-transcriber
+recognizer=${basedir}/espnet
+workdir=${segmenter}/build
+model=/home/users2/denisopl/arbeitsdaten/models/${language}
+
+# Segment the audio
+mkdir -p ${workdir}/audio/base
+sox ${file} -c 1 -b 16 ${workdir}/audio/base/${recid}.wav rate -v 16k
+cd ${segmenter}
+make build/trans/${recid}/utt2spk
+sed -i -e 's! build/! '${workdir}'/!g' build/trans/${recid}/wav.scp
+nj=$(wc -l ${workdir}/trans/${recid}/segments | awk '{print $1}')
+awk '{print $1" example"}' ${workdir}/trans/${recid}/segments > ${workdir}/trans/${recid}/text
+cd ${basedir}
+
+# Recognize the speech
+cd ${recognizer}/egs/multi_${language}/asr1/
+. path.sh
+utils/fix_data_dir.sh ${workdir}/trans/${recid}
+steps/make_fbank_pitch.sh --cmd run.pl --nj ${nj} --write_utt2num_frames true ${workdir}/trans/${recid} ${workdir}/feats/${recid}
+utils/fix_data_dir.sh ${workdir}/trans/${recid}
+dumpdir=${workdir}/dump/${recid}
+dump.sh --cmd run.pl --nj ${nj} --do_delta false ${workdir}/trans/${recid}/feats.scp ${model}/cmvn.ark ${workdir}/exp/${recid} ${dumpdir}
+data2json.sh --feat ${dumpdir}/feats.scp --bpecode ${model}/bpe.model ${workdir}/trans/${recid} ${model}/units.txt > ${dumpdir}/data.json
+splitjson.py --parts ${nj} ${dumpdir}/data.json
+
+run.pl JOB=1:${nj} ${dumpdir}/log/decode.JOB.log \
+	asr_recog.py \
+		--ngpu 0 \
+		--backend pytorch \
+		--recog-json ${dumpdir}/split${nj}utt/data.JOB.json \
+		--result-label ${dumpdir}/result.JOB.json \
+		--model ${model}/asr/model.dat  \
+		--beam-size 20 \
+		--penalty 0.0 \
+		--maxlenratio 0.0 \
+		--minlenratio 0.0 \
+		--ctc-weight 0.5 \
+		--rnnlm ${model}/lm/model.dat \
+		--lm-weight 0.5
+
+concatjson.py ${dumpdir}/result.*.json > ${dumpdir}/result.json
+json2trn.py ${dumpdir}/result.json ${model}/units.txt ${dumpdir}/ref.trn ${dumpdir}/hyp.trn
+sed -i.bak2 -r 's/<blank> //g' ${dumpdir}/hyp.trn
+filt.py -v ${model}/non_lang_syms.txt ${dumpdir}/hyp.trn > ${dumpdir}/hyp.trn.filtered
+spm_decode --model=${model}/bpe.model --input_format=piece < ${dumpdir}/hyp.trn.filtered | sed -e "s/▁/ /g" > ${dumpdir}/hyp.wrd.trn
+
+perl -p -e 's/(.+)\((.+)\)$/$2 $1/g' ${dumpdir}/hyp.wrd.trn | sort > ${file}.txt
+rm -rf ${workdir}/audio/base/${recid}.wav ${workdir}/*/${recid}
---
+++ server.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import asyncio
+import json
+import websockets
+import subprocess
+import os
+import urllib
+import hashlib
+import fcntl
+
+connected = dict()
+
+async def decode(websocket, path):
+    model = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(path).query))['model']
+    datadir = os.path.dirname(os.path.realpath(__file__)) + '/data'
+    os.makedirs(datadir, exist_ok=True)
+
+    data = connected[websocket]['audio']
+    recid = hashlib.sha3_256(data).hexdigest()
+    datafile = datadir + '/' + recid + '.' + connected[websocket]['options']['content-type'].split('/')[-1]
+    textfile = datafile + '.txt'
+    lockfile = datafile + '.lock'
+
+    with open(lockfile, 'wb') as lf:
+        fcntl.flock(lf, fcntl.LOCK_EX)
+
+        if not os.path.isfile(textfile):
+            with open(datafile, 'wb') as df:
+                df.write(data)
+
+            subprocess.run(
+                f'echo ./decode.sh {datafile} {model} | /mount/arbeitsdaten41/projekte/asr-2/denisopl/gentoo-sumpfweihe/startprefix',
+                shell=True, capture_output=True
+            )
+
+        fcntl.flock(lf, fcntl.LOCK_UN)
+
+    rindex = 0
+
+    with open(textfile, encoding='utf-8') as f:
+        for l in f:
+            transcript = ''
+            results = {
+                'results': [
+                    {
+                        'word_alternatives': [],
+                        'keywords_result': {},
+                        'alternatives': [{'timestamps': [], 'confidence': 1.0}],
+                        'final': True
+                    }
+                ],
+                'result_index': rindex
+            }
+
+            words = l.strip().split(' ')
+            start = float(words[0][-17:-9])
+            end = float(words[0][-8:])
+
+            for word in words[1:]:
+                transcript += ' ' + word
+
+                results['results'][0]['word_alternatives'].append({
+                    'start_time': start,
+                    'end_time': end,
+                    'alternatives': [{'confidence': 1.0, 'word': word}]
+                })
+
+                results['results'][0]['alternatives'][0]['timestamps'].append([word, start, end])
+
+            results['results'][0]['alternatives'][0]['transcript'] = transcript.strip()
+
+            await websocket.send(json.dumps(results))
+            rindex += 1
+
+async def handler(websocket, path):
+    # Register.
+    connected[websocket] = dict()
+    connected[websocket]['audio'] = b''
+
+    try:
+        async for message in websocket:
+            if type(message) is str:
+                m = json.loads(message)
+                if m['action'] == 'stop':
+                    await decode(websocket, path)
+                    break
+                else:
+                    if m['action'] == 'start':
+                        connected[websocket]['options'] = m
+                    await websocket.send('{"state":"listening"}')
+
+
+            elif type(message) is bytes:
+                connected[websocket]['audio'] += message
+
+    finally:
+        # Unregister.
+        del connected[websocket]
+
+start_server = websockets.serve(handler, 'aztekendrossel.ims.uni-stuttgart.de', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
