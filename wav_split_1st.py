62fdae0bbedf87a2e358aa3feb7947ba85a61d75
Ubuntu <mwkoo9@gmail.com>
2020-09-23 22:51:34 +0900

    Refactor wav_split_1st.py, by J.P

--- wav_split_1st.py
+++ wav_split_1st.py
@@ -5,18 +5,7 @@ import shutil
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-# file = '/Users/jplee/Dropbox/2020년/2020.05_그랜드챌린지/데이터/sample_data_16k/file003_e.wav'
-datadirIn='./sample_data_16k/'
-datadirOut='../espnet/egs/korean/asr3/downloads/KoreanSpeech/test/test_01/'
-wavtype='.wav'
-#file = './sample_data_16k/file001_e.wav'
-#file = 'file001_n'
-#Dir. for Output file
-fileInDir='./sampleOut/'
-fileIn = sys.argv[1]
-print (fileInDir + fileIn)
-#file = './sample_data_16k/file002_e.wav'
-#file = './sample_data_16k/file003_e.wav'
+dir_path = './sample_data_16k/playlist_xx'
 
 def save_to_json(data, filename='data.json'):
     if filename[-4:] != 'json':
@@ -24,7 +13,7 @@ def save_to_json(data, filename='data.json'):
 
     with open(f'{filename}', 'w', encoding='utf-8') as fw:
         json.dump(data, fw, indent=4, ensure_ascii=False)
-        
+ 
 def createFolder(directory):
     try:
         if not os.path.exists(directory) :
@@ -37,7 +26,7 @@ def createFolder(directory):
         print("Error: Creating directory" + directory)
 
 
-def split_audio(file, dest_path, dest_pathOrg='./sample_data_16k'):
+def split_audio(file, dest_path):
 
     vadcommand = "./vadR1.sh  " + file
     print(vadcommand)
@@ -46,8 +35,8 @@ def split_audio(file, dest_path, dest_pathOrg='./sample_data_16k'):
     # REading segmentation Result
     Segfile = open("./temp/segmentation/output_seg/segments",'r')
     sound_file = AudioSegment.from_wav(file)
-    positionsWave = []
-    positionsFlac = []
+    outputs = []
+    
     i=0
     while True:
         line = Segfile.readline()
@@ -55,55 +44,55 @@ def split_audio(file, dest_path, dest_pathOrg='./sample_data_16k'):
             break
         words = line.split()
 
-        out_filewav = os.path.join(dest_pathOrg, "chunk_{:02d}.wav".format(i))
-        out_fileflac = os.path.join(dest_path, "chunk_{:02d}.flac".format(i))
-        out_filetxt = os.path.join(dest_path, "chunk_{:02d}.txt".format(i))
-        #print(out_file, words[2], words[3])
-        positionsWave.append((out_filewav,words[2], words[3]))
-        positionsFlac.append((out_fileflac,words[2], words[3]))
+        out_filewav = os.path.join(dest_path, "chunk_{:03d}.wav".format(i))
+        
         startmsec= int(float(words[2])*1000)
         endmsec = int(float(words[3])*1000)
 
         seg_sound = sound_file[startmsec:endmsec]
         seg_sound.export(out_filewav, format="wav")
-        seg_sound.export(out_fileflac, format="flac")
-        with open(out_filetxt,"w") as ftxt :
-            ftxt.write("실험 평가\n")
+            
+        output = {}
+        output['file'] = out_filewav
+        output['start'] = str(words[2])
+        output['end'] = str(words[3])
+        
+        outputs.append(output)
+        
         i = i+1
+        
     Segfile.close()
 
-    return positionsWave
-
-dest_path = datadirOut + fileIn + '01'
-createFolder(datadirOut)
-inputfile= datadirIn + fileIn + wavtype
-print('inputfile=',inputfile,'dest_path=',dest_path)
-
-createFolder(dest_path)
-
-# Dir for wave file
-des_dir_wav=fileInDir + fileIn +'Dir'
-createFolder(des_dir_wav)
-
-positions = split_audio(inputfile,dest_path,des_dir_wav)
-nj = len(positions)
-
-for file,start, end in positions:
-    print(file,"  ",f'{start} - {end}')
+    return outputs
 
 
-output_list = []
-for file,start, end in positions:
-    elem = {}
-    elem['file'] = file
-    elem['start'] = str(start)
-    elem['end'] = str(end)
-    
-    output_list.append(elem)
-    
-resultfilenameOut = fileInDir + fileIn + 'Out.json'
-save_to_json(output_list, resultfilenameOut)
+files = os.listdir(dir_path)
+files = sorted(files)
 
+for file in files:
+    curdir = dir_path
+    filename, file_extension = os.path.splitext(file)
+    if file_extension != '.wav':
+        continue
 
+    # Dir for wave file
+    dest_path = '/tmp/wav_split'
+    createFolder(dest_path)
+
+    inputfile = os.path.join(curdir, file)
+
+    outputs = split_audio(inputfile, dest_path)
+
+    for output in outputs:
+        file = output['file']
+        start = output['start']
+        end = output['end']
+        
+        print(f'{file}\t{start} - {end}')
+        
+    json_file = os.path.join(curdir, f'{filename}.json')
+    save_to_json(outputs, json_file)
+
+print('Finished!')
 
 
